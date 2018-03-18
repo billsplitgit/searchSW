d1e419c1863aff034c72d286ac8a7ee43aef6523
harshit <harshit@example.com>
2018-03-18 23:22:37 +0530

    2nd commit. search implemented partailly

--- src/app/land-here/land-here.component.ts
+++ src/app/land-here/land-here.component.ts
@@ -11,7 +11,8 @@ export class LandHereComponent implements OnInit {
 
   constructor(private _getData: GetDataService) { }
   filterPlanet: string;
-  planets: Planets;
+  planets: Planets[] ;
+  filteredPlanets: Planets[] = [];
   getPlanets() {
     this._getData.getPlanets().subscribe(data => {
       this.planets = data["results"];
@@ -19,9 +20,22 @@ export class LandHereComponent implements OnInit {
     });
   }
 
-  searchPlanets(){
-
+  searchPlanets(val){
+    this._getData.getPlanets().subscribe(data => {
+      for(let i=0; i<data['results'].length; i++){
+        
+        if(data['results'][i].name.indexOf(val)>0){
+          this.filteredPlanets.push(data['results'][i]);
+          this.filteredPlanets.sort((a, b) => {
+                return a < b ? -1 : 1;
+             });
+          console.log(this.filteredPlanets);
+        }
+      }
+      
+    }); 
   }
+
   ngOnInit() {
     this.getPlanets();
   }
